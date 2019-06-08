94f6c00d468cbda3fb83c6ba6edb61a3e71134bf
Michael Redig <mredig@gmail.com>
2019-06-08 15:11:07 -0500

    replicated perl script into swift now

--- Sources/gcloud_watcher/main.swift
+++ Sources/gcloud_watcher/main.swift
@@ -1,1 +1,51 @@
-print("Hello, world!")
+import Foundation
+
+
+func main() {
+	let args = getArgs()
+	let allFiles = scrapeDirectory(at: args.directory)
+	print(allFiles)
+}
+
+func scrapeDirectory(at directory: URL) -> [URL] {
+	let fm = FileManager.default
+
+	print("scraping \(directory)")
+	do {
+		let contents = try fm.contentsOfDirectory(at: directory, includingPropertiesForKeys: [URLResourceKey.fileSizeKey], options: .skipsHiddenFiles)
+		return contents
+	} catch {
+		fatalError("There was an error scraping the directory \(directory): \(error)")
+	}
+}
+
+func getArgs() -> (directory: URL, nothing: String) {
+	guard CommandLine.argc > 1 else {
+		fatalError(printInstructions(false))
+	}
+
+	var directoryString = ""
+	for index in 0..<CommandLine.argc {
+		let index = Int(index)
+		let argument = CommandLine.arguments[index]
+		if index == 1 {
+			directoryString = argument
+		}
+	}
+
+	let directory = URL(fileURLWithPath: directoryString)
+	return (directory, "")
+}
+
+
+@discardableResult func printInstructions(_ printOut: Bool = true) -> String {
+	let instructions = """
+Usage: watcher [pathToWatchDirectory]
+"""
+	if printOut {
+		print(instructions)
+	}
+	return instructions
+}
+
+main()
